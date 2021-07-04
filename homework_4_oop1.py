4c8fb376451e2ecc697fa8f5167c6c1a47194bcd
Bohdan Kryven <bodyan1108@gmail.com>
2021-07-04 23:05:14 +0300

    added third point

--- homework_4_oop1.py
+++ homework_4_oop1.py
@@ -1,5 +1,4 @@
 # 1. Create a Vehicle class with max_speed and mileage instance attributes
-print("1. Create a Vehicle class with max_speed and mileage instance attributes")
 
 
 class Vehicle:
@@ -10,12 +9,15 @@ class Vehicle:
 
 # 2. Create a child class Bus that will inherit all of the variables and methods of the Vehicle class and will have
 # seating_capacity own method
-print("""2. Create a child class Bus that will inherit all of the variables 
-and methods of the Vehicle class and will have seating_capacity own method 
-""")
 
 
 class Bus(Vehicle):
     def __init__(self, seating_capacity, max_speed, mileage_instance):
         super().__init__(max_speed, mileage_instance)
         self.seating_capacity = seating_capacity
+
+
+# 3. Determine which class a given Bus object belongs to (Check type of an object)
+print("Checking type of an object (class Buss):", type(Vehicle))
+print(isinstance(Bus, type))
+
