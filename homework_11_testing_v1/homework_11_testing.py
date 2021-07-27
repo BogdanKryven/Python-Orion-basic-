bd672c1cc2533745507a9c6ad813e91d0987c050
Bohdan Kryven <bodyan1108@gmail.com>
2021-07-27 14:08:05 +0300

    added finished homework

--- homework_11_testing_v1/homework_11_testing.py
+++ homework_11_testing_v1/homework_11_testing.py
@@ -1,8 +1,10 @@
+class SqrtException(Exception):
+    pass
 
 
 class Calc:
     @staticmethod
-    def sum(a, b) -> int:
+    def sum(a, b):
         """ Шукає суму двох чисел
 
         >>> Calc.sum(3, 2)
@@ -12,82 +14,113 @@ class Calc:
         :param b: Друге число
         :return: Результат
         """
-        return a + b
+
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            return a + b
+
+        else:
+            print("You must enter only integer or float type numbers")
+
 
     @staticmethod
-    def minus(a, b):
+    def diff(a, b):
         """ Шукає різницю двох чисел
 
-                >>> Calc.minus(3, 2)
-                1
+        >>> Calc.diff(3, 2)
+        1
 
-                :param a: Перше число
-                :param b: Друге число
-                :return: Результат
-                """
-        return a - b
+        :param a: Перше число
+        :param b: Друге число
+        :return: Результат
+         """
+
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            return a - b
+        else:
+            print("You must enter only integer or float type numbers")
 
     @staticmethod
     def mul(a, b):
         """ Шукає добуток двох чисел
 
-                >>> Calc.mul(3, 2)
-                6
+        >>> Calc.mul(3, 2)
+        6
 
-                :param a: Перше число
-                :param b: Друге число
-                :return: Результат
-                """
-        return a * b
+        :param a: Перше число
+        :param b: Друге число
+        :return: Результат
+        """
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            return a * b
+        else:
+            print("You must enter only integer or float type numbers")
 
     @staticmethod
     def div(a, b):
         """ Шукає частку двох чисел
 
-                >>> Calc.div(3, 2)
-                1.5
+        >>> Calc.div(3, 2)
+        1.5
 
-                :param a: Перше число
-                :param b: Друге число
-                :return: Результат
-                """
+        :param a: Перше число
+        :param b: Друге число
+        :return: Результат
+        """
+
+        # if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        #     # return a / b
+        #     try:
+        #         div = a / b
+        #     except ZeroDivisionError:
+        #         return "You can not divide by 0"
+        # else:
+        #     # print("You must enter only integer or float type numbers")
+        #     return "You must enter only integer or float type numbers"
         return a / b
+
+
+
+
 
     @staticmethod
     def pow(a, b):
         """ Піднімає печше число до степеня другого числа
 
-                >>> Calc.pow(3, 2)
-                9
+        >>> Calc.pow(3, 2)
+        9
 
-                :param a: Перше число (яке підносимо до степеня)
-                :param b: Друге число (степінь до якого ми підносимо це число)
-                :return: Результат
-                """
+        :param a: Перше число (яке підносимо до степеня)
+        :param b: Друге число (степінь до якого ми підносимо це число)
+        :return: Результат
+        """
         return pow(a, b)
 
     @staticmethod
     def sqrt(a, b) -> float:
         """ Взяття з під кореня
 
-                >>> Calc.sqrt(16, 4)
-                2.0
+        >>> Calc.sqrt(16, 4)
+        2.0
 
-                :param a: Перше число (яке під коренем)
-                :param b: Друге число (корінь якого степеня)
-                :return: Результат
-                """
-        return round(a ** (1 / b), 5)
+        :param a: Перше число (яке під коренем)
+        :param b: Друге число (корінь якого степеня)
+        :return: Результат
+        """
+
+        if a < 0:
+            raise SqrtException
+        return a ** (1 / b)
+
 
     @staticmethod
     def percentage_of_number(a, b) -> float:
         """ Шукає суму двох чисел
 
-                >>> Calc.percentage_of_number(3, 100)
-                3.0
+        >>> Calc.percentage_of_number(3, 100)
+        3.0
 
-                :param a: Перше число (число, яке ми прирівнюємо до іншого числа)
-                :param b: Друге число (від якого і шукаємо відсоток)
-                :return: Результат
-                """
+        :param a: Перше число (число, яке ми прирівнюємо до іншого числа)
+        :param b: Друге число (від якого і шукаємо відсоток)
+        :return: Результат
+        """
         return round(((a / b) * 100), 2)
