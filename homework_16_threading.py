c81db1d1cb64b553624d1418afbac13eacfc9ece
Bohdan Kryven <bodyan1108@gmail.com>
2021-08-13 17:13:21 +0300

    added finished homework

--- homework_16_threading.py
+++ homework_16_threading.py
@@ -1,61 +1,49 @@
 import threading
-from threading import Thread
 from math import sqrt
+import logging
+import time
 
 
-def decorator_thread(func):
-    def wrapper(*args):
-        process_1 = Thread(target=func,
-                           args=args)
-        process_2 = Thread(target=func,
-                           args=args)
-        processes = [process_1, process_2]
-        for i in processes:
-            i.start()
-            print(threading.get_ident())
-            i.join()
+def decorator_time(func):
+    def wrapper(*args, **kwargs):
+        bef = time.monotonic()
+        func(*args, **kwargs)
+        time.sleep(1)
+        logging.info(f"Time required: {time.monotonic() - bef}")
+
     return wrapper
 
 
-@decorator_thread
-def quadratic_equation(a, b, c) -> str:
+@decorator_time
+def quadratic_equation(name_thread_, a, b, c):
+    logging.info(f"Thread {name_thread_} starting!")
     d = pow(b, 2) - 4 * a * c
     if d > 0:
         results = (-b - sqrt(d)) / 2 * a, (-b + sqrt(d)) / 2 * a
-        return f"The roots of quadratic equation " \
-               f"{a}x^2 {'+' if b > 0 else '-'} " \
-               f"{b if b > 0 else -b}x {'+' if c > 0 else '-'} " \
-               f"{c if c > 0 else -c} = {results}"
+        logging.info(f"The roots of quadratic equation "
+                     f"{a}x^2 {'+' if b > 0 else '-'} "
+                     f"{b if b > 0 else -b}x {'+' if c > 0 else '-'} "
+                     f"{c if c > 0 else -c} = {results}")
     elif d == 0:
         result = -b / 2 * a
-        return f"The root quadratic equation " \
-               f"{a}x^2 {'+' if b > 0 else '-'} " \
-               f"{b if b > 0 else -b}x {'+' if c > 0 else '-'} " \
-               f"{c if c > 0 else -c} = {result}"
+        logging.info(f"The root quadratic equation "
+                     f"{a}x^2 {'+' if b > 0 else '-'} "
+                     f"{b if b > 0 else -b}x {'+' if c > 0 else '-'} "
+                     f"{c if c > 0 else -c} = {result}")
     else:
-        return "No roots in quadratic equation"
+        logging.info("No roots in quadratic equation")
+    logging.info(f"Thread {name_thread_} finishing!")
 
 
-print(quadratic_equation(float(input("a = ")), float(input("b = ")), float(input("c = "))))
+format_ = "%(asctime)s : %(message)s"
+logging.basicConfig(format=format_, level=logging.INFO)
 
-# def decorator_thread():
-#     process_1 = Thread(target=quadratic_equation,
-#                        args=(int(float(input("a = "))), int(float(input("b = "))), int(float(input("c = ")))))
-#     process_2 = Thread(target=quadratic_equation,
-#                        args=(int(float(input("a = "))), int(float(input("b = "))), int(float(input("c = ")))))
-#     processes = [process_1, process_2]
-#     for i in processes:
-#         i.start()
-#         print(threading.get_ident())
-#         i.join()
-
-
-# process_11 = Thread(target=quadratic_equation,
-#                     args=(int(float(input("a = "))), int(float(input("b = "))), int(float(input("c = ")))))
-# print(process_11)
-# process_21 = Thread(target=quadratic_equation,
-#                     args=(int(float(input("x = "))), int(float(input("y = "))), int(float(input("z = ")))))
-# print(process_11)
-# process_31 = Thread(target=quadratic_equation,
-#                     args=(int(float(input("x = "))), int(float(input("y = "))), int(float(input("z = ")))))
-# print(process_31)
+first = threading.Thread(target=quadratic_equation,
+                         args=(1, 1, 4, 4))
+second = threading.Thread(target=quadratic_equation,
+                          args=(2, 1, 7, 3))
+tasks = [first, second]
+for i in tasks:
+    i.start()
+    i.join()
+    logging.info(f"Indent THREAD: {threading.get_ident()}\n")
